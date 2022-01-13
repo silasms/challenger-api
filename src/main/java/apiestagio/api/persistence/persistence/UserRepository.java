059377f9e07cab60549d80a78924cc749fbe078c
silasms <silas.medeiros7@gmail.com>
2022-01-13 20:35:02 -0300

    refactor: cleaning imports

--- src/main/java/apiestagio/api/persistence/persistence/UserRepository.java
+++ src/main/java/apiestagio/api/persistence/persistence/UserRepository.java
@@ -1,7 +1,6 @@
 package apiestagio.api.persistence.persistence;
 
 import apiestagio.api.persistence.entity.UserEntity;
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
