b4e6039734208c3089073a39d51ab103518fa576
silasms <silas.medeiros7@gmail.com>
2022-01-13 20:32:51 -0300

    chore: adding new dependencies

---
+++ src/test/java/apiestagio/api/UserTestConfig.java
@@ -0,0 +1,11 @@
+package apiestagio.api.test;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles(profiles = "local")
+@AutoConfigureMockMvc
+public class UserTestConfig {
+}
